0ba6589acbb3ad087664d6b10cf66dde7a6b5479
vincent <vpark45@gmail.com>
2018-04-10 15:20:36 +0900

    first commit

---
+++ file.py
@@ -0,0 +1,21 @@
+from urllib.request import Request, urlopen
+from urllib.parse import urlencode
+from json import loads
+
+def report(apikey, resource):
+	'''Retrieve file scan reports
+	return type is dictionary'''
+	url = 'https://www.virustotal.com/vtapi/v2/file/report'
+	params = {'apikey': apikey, 'resource': resource}
+	data = urlencode(params).encode('ascii')
+	req = Request(url, data=data)
+	res = urlopen(req)
+	code = res.getcode()
+
+	if code != 200:
+		print('Status code is not 200')
+		exit()
+
+	json = res.read().decode('utf-8')
+	res.close()
+	return loads(json)
